8f5aa741228185c0155d312ee12cab40e5b2ab28
Steven Munk <s.f.munk@gmx.de>
2022-09-19 15:42:48 +0200

    Latergram

--- html.js
+++ html.js
@@ -1,81 +1,145 @@
-function createAPost(post) {
+function createAPost(post, randomLikes, randomComments) {
     
     document.getElementById('all-posts').innerHTML += `
 
         <post>
 
-                <post-head class="post-head dpf">
+            <post-head class="post-head dpf">
 
-                    <div class="post-profile dpf">
+                <div class="post-profile dpf">
 
-                        <div class="post-profile-pic-outline dpf">
-                            <img class="post-profile-pic" src="${post['profilePic']}">
+                    <div class="post-profile-pic-outline dpf">
+                        <img class="post-profile-pic" src="${post['profilePic']}">
+                    </div>
+
+                    <h3 class="post-username">${post['username']}</h3>
+
+                </div>
+                <img class="post-settings" src="icon/icons8-mehr-32.png">
+            
+            </post-head>
+
+            <img class="post-img" src="${post['image']}">
+
+            <div class="post-caption dpf">
+
+                <div class="post-caption-margin dpf">
+
+                    <div class="post-icons dpf">
+
+                        <div>
+                            <img class="post-single-icon" src="icon/icons8-herzen-32.png">
+                            <img class="post-single-icon" src="icon/icons8-sprechblase-32.png">
+                            <img class="post-single-icon" src="icon/icons8-in-meiner-nähe-32.png">
                         </div>
-
-                        <h3 class="post-username">${post['username']}</h3>
+                        <img class="post-single-icon" src="icon/icons8-lesezeichen-32.png">
 
                     </div>
-                    <img class="post-settings" src="icon/icons8-mehr-32.png">
-                
-                </post-head>
 
-                <img class="post-img" src="${post['image']}">
+                    <h3 class="post-likes">
+                        Gefällt ${randomLikes} Mal
+                    </h3>
 
-                <div class="post-caption dpf">
-
-                    <div class="post-caption-margin dpf">
-
-                        <div class="post-icons dpf">
-
-                            <div>
-                                <img class="post-single-icon" src="icon/icons8-herzen-32.png">
-                                <img class="post-single-icon" src="icon/icons8-sprechblase-32.png">
-                                <img class="post-single-icon" src="icon/icons8-in-meiner-nähe-32.png">
-                            </div>
-                            <img class="post-single-icon" src="icon/icons8-lesezeichen-32.png">
-
-                        </div>
-
-                        <h3 class="post-likes">
-                            Gefällt 28,010 Mal
-                        </h3>
-
-                        <span>
-                            <p class="post-description">
-                                <b>${post['username']}</b>
-                                ${post['caption']}
-                            </p>
-                        </span>
-
-                        <p class="post-all-comments">
-                            Alle 193 Kommentare ansehen
+                    <span>
+                        <p class="post-description">
+                            <b>${post['username']}</b>
+                            ${post['caption']}
                         </p>
+                    </span>
 
-                        <span>
-                            <p class="post-upload">
-                                GESTERN
-                                <b class="post-translation">Übersetzung anzeigen</b>
-                            </p>
-                        </span>
+                    <p class="post-all-comments">
+                        Alle ${randomComments} Kommentare ansehen
+                    </p>
 
-                    </div>
+                    <span id="add-comment" class="whole-comment dpf">
+                    </span>
+
+                    <span>
+                        <p class="post-upload">
+                            GESTERN
+                            <b class="post-translation">Übersetzung anzeigen</b>
+                        </p>
+                    </span>
 
                 </div>
 
-                <comment class="comment-container dpf">
-                    <div class="comment-input-container dpf">
-                        <img class="emoji-icon" src="icon/icons8-glücklich-32.png">
-                        <input class="post-comment-input" type="text" placeholder="Kommentieren ...">
-                    </div>
-                    <p class="post-comment-btn">
-                        Posten
-                    </p>
-                </comment>
+            </div>
 
-            </post>
+            <form class="comment-container dpf" onsubmit="createAComment(); return false;">
+                <div class="comment-input-container dpf">
+                    <img class="emoji-icon" src="icon/icons8-glücklich-32.png">
+                    <input required id="comment" class="post-comment-input" type="text" placeholder="Kommentieren ..." required>
+                </div>
+                <input value="Posten" type="submit" class="post-comment-btn">
+            </form>
+
+            
 
         <post>
 
+    `;
+
+}
+
+
+
+function createOneStory(post) {
+
+    document.getElementById('all-stories').innerHTML += `
+    
+    <story class="story dpf">
+        <div class="story-profile-pic-outline dpf">
+            <img class="story-profile-pic" src="${post['profilePic']}">
+        </div>
+        <p class="story-profile-name">${post['username']}</p>
+    </story>
+    
+    `;
+
+}
+
+
+
+function createASuggestion(post) {
+
+    document.getElementById('sugesstions').innerHTML += `
+    
+    <div class="logged-in-profile dpf">
+
+        <div class="logged-in-left dpf">
+
+            <img class="logged-in-stranger-profile-pic" src="${post['profilePic']}">
+
+            <div>
+                <p class="logged-in-username">${post['username']}</p>
+                <p class="logged-in-real-name">${post['name']}</p>
+            </div>
+
+        </div>
+
+        <p class="logged-in-change-follow-btn">Folgen</p>
+
+    </div>
+    
+    `;
+
+}
+
+
+
+function createLoggedInUser() {
+
+    const post = posts[0];
+
+    document.getElementById('looged-in-user').innerHTML = `
+    
+    <img class="logged-in-profile-pic" src="${post['profilePic']}">
+
+    <div>
+        <p class="logged-in-username">${post['username']}</p>
+        <p class="logged-in-real-name">${post['name']}</p>
+    </div>
+    
     `;
 
 }
--- script.js
+++ script.js
@@ -1,39 +1,45 @@
 let posts = [
 
     {
+        'name': 'Steven Munk',
         'username': 'Icetune',
         'image': 'img/post/pexels-polina-chistyakova-13421007.jpg',
-        'caption': 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam',
+        'caption': 'Ignoriert man den Mercedes auf der linken Seite des Bildes, oder schneidet ihn mittels PS aus dem Bild raus, könnt man denken dass das ein sehr hoch aufgelöstes Bild aus RDR2 ist :D',
         'profilePic': 'img/profile/Steven.PNG'
     },
     {
-        'username': 'username1',
+        'name': 'Sandra Armani',
+        'username': 'Sandra3105',
         'image': 'img/post/pexels-aubin-kirch-13406218.jpg',
-        'caption': 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam',
+        'caption': 'Hallo meine Lieben :) Ihr wisst ja ich werde demnächst aufs Land ziehen und werde mein Fahrrad nicht mehr brauchen, bei Intresse bitte melden. Das Fahrrad steht zum Verkauf!',
         'profilePic': 'img/profile/pexels-andrea-piacquadio-774909.jpg'
     },
     {
-        'username': 'username2',
+        'name': 'Philipp Bruns',
+        'username': 'Einfach_Phil',
         'image': 'img/post/pexels-blue-arauz-13527050.jpg',
-        'caption': 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam',
+        'caption': 'Heute einen erfolgreichen Geschäftstag im Theather in Chicago gehabt, um was es ging? Dazu mehr in ein paar Wochen wenn das Ding steht!',
         'profilePic': 'img/profile/pexels-andrew-personal-training-697509.jpg'
     },
     {
-        'username': 'username3',
+        'name': 'Josefine Rolof',
+        'username': 'Josieeeeee',
         'image': 'img/post/pexels-eberhard-grossgasteiger-12527034.jpg',
-        'caption': 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam',
+        'caption': 'Ich habe gehört in diesem Haus soll Batman wohnen?! Kleiner Spas am Rande, aber mal ernsthaft, IST DAS NICHT EIN HAMMER BILD OMG!!!',
         'profilePic': 'img/profile/pexels-juliana-stein-1898555.jpg'
     },
     {
-        'username': 'username4',
+        'name': 'Thomas Murr',
+        'username': 'Xx_Thomas_xX',
         'image': 'img/post/pexels-lucas-andreatta-13348025.jpg',
-        'caption': 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam',
+        'caption': '"Strand an, Kopf aus..." - Ich selber.  Es ist schön für ein paar Tage aus dem Alltagsstress rauszukommen und den kantigen Verstand durch das glättende Wasser zu ziehen...',
         'profilePic': 'img/profile/pexels-pixabay-220453.jpg'
     },
     {
-        'username': 'username5',
+        'name': 'Tara Schwarz',
+        'username': 'Tara0101',
         'image': 'img/post/pexels-mati-mango-5215483.jpg',
-        'caption': 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam',
+        'caption': 'Ich liebe Flugzeuge ja und heute, JA HEUTE, habe ich das legendäre Flugzeug von Captain Balu zu sehen bekommen!!! Balu und seine Crew!!!',
         'profilePic': 'img/profile/pexels-pixabay-415829.jpg'
     },
 
@@ -41,16 +47,50 @@ let posts = [
 
 
 
-function show() {
+function renderPage() {
 
     document.getElementById('all-posts').innerHTML = '';
+    document.getElementById('all-stories').innerHTML = '';
+    document.getElementById('sugesstions').innerHTML = '';
+    document.getElementById('looged-in-user').innerHTML = '';
 
-    for (let i = 0; i < 6; i++) {
+    for (let i = 0; i < 1; i++) {
         const post = posts[i];
 
-        createAPost(post);
+        let randomLikes = Math.floor(Math.random() * 1000);
+        let randomComments = Math.floor(Math.random() * 500);
+
+        createOneStory(post);
+        createAPost(post, randomLikes, randomComments);
         
     }
+
+    createLoggedInUser();
+
+    for (let i = 1; i < posts.length; i++) {
+        const post = posts[i];
+
+        createASuggestion(post);
+        
+    }
+
 }
 
 
+
+function createAComment() {
+
+    let x = document.getElementById(`comment`).value;
+
+    document.getElementById(`add-comment`).innerHTML += `
+    
+    <div class="comment-space-b dpf">
+    <p class="comment-text"><b class="comment-username">Du </b>
+    ${x}</p>
+
+    <img class="little-like" src="icon/icons8-herzen-32.png">
+    </div>
+    
+    `;
+
+}
